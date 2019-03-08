da9aec4faf084e2176bbc7c315692e4490775b9e
aereal <aereal@aereal.org>
2019-03-09 00:20:46 +0900

    Introduce context to tests

--- src/mackerel-container-agent-definition.test.ts
+++ src/mackerel-container-agent-definition.test.ts
@@ -3,50 +3,53 @@ import { Stack } from "@aws-cdk/cdk"
 import { MackerelContainerAgentDefinition } from "./mackerel-container-agent-definition"
 
 describe("MackerelContainerAgentDefinition", () => {
-  test("add to task definition with only required props", () => {
-    const stack = new Stack()
-    const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
-    const container = new MackerelContainerAgentDefinition(
-      stack,
-      "mackerel-container-agent",
-      {
-        apiKey: "keep-my-secret",
-        taskDefinition,
-      }
-    )
-    expect(stack.toCloudFormation()).toMatchSnapshot()
+  describe("EC2", () => {
+    test("add to task definition with only required props", () => {
+      const stack = new Stack()
+      const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
+      const container = new MackerelContainerAgentDefinition(
+        stack,
+        "mackerel-container-agent",
+        {
+          apiKey: "keep-my-secret",
+          taskDefinition,
+        }
+      )
+      expect(stack.toCloudFormation()).toMatchSnapshot()
+    })
+
+    test("with roles", () => {
+      const stack = new Stack()
+      const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
+      const container = new MackerelContainerAgentDefinition(
+        stack,
+        "mackerel-container-agent",
+        {
+          apiKey: "keep-my-secret",
+          roles: [
+            { service: "My-service", role: "db" },
+            { service: "My-service", role: "proxy" },
+          ],
+          taskDefinition,
+        }
+      )
+      expect(stack.toCloudFormation()).toMatchSnapshot()
+    })
+
+    test("specify ignoreContainer", () => {
+      const stack = new Stack()
+      const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
+      const container = new MackerelContainerAgentDefinition(
+        stack,
+        "mackerel-container-agent",
+        {
+          apiKey: "keep-my-secret",
+          ignoreContainer: "(mackerel|xray)",
+          taskDefinition,
+        }
+      )
+      expect(stack.toCloudFormation()).toMatchSnapshot()
+    })
   })
 
-  test("with roles", () => {
-    const stack = new Stack()
-    const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
-    const container = new MackerelContainerAgentDefinition(
-      stack,
-      "mackerel-container-agent",
-      {
-        apiKey: "keep-my-secret",
-        roles: [
-          { service: "My-service", role: "db" },
-          { service: "My-service", role: "proxy" },
-        ],
-        taskDefinition,
-      }
-    )
-    expect(stack.toCloudFormation()).toMatchSnapshot()
-  })
-
-  test("specify ignoreContainer", () => {
-    const stack = new Stack()
-    const taskDefinition = new Ec2TaskDefinition(stack, "TaskDefinition", {})
-    const container = new MackerelContainerAgentDefinition(
-      stack,
-      "mackerel-container-agent",
-      {
-        apiKey: "keep-my-secret",
-        ignoreContainer: "(mackerel|xray)",
-        taskDefinition,
-      }
-    )
-    expect(stack.toCloudFormation()).toMatchSnapshot()
-  })
 })
